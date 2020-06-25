17442ceca4d3eff6c6369e997400f678145bc11a
Yampier Medina <jpmti2016@gmail.com>
2020-06-25 09:02:09 -0400

    Add cssnano to minify css

--- postcss.config.js
+++ postcss.config.js
@@ -8,5 +8,8 @@ module.exports = {
         content: ["index.html"],
         defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
       }),
+    require("cssnano")({
+      preset: "default",
+    }),
   ],
 };
